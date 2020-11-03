e5688c2a437d3ee079bb04d12864fdc3e48a3bcd
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-03 12:16:15 -0600

    finished, although employee validation causing problem with seed.rb

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,31 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+    @dogs = @dogs.sort{|dog1, dog2| dog1.employees.count <=> dog2.employees.count}.reverse
+  end
+
+  def show
+    @dog = Dog.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :age, :breed)
+  end
+
 end
